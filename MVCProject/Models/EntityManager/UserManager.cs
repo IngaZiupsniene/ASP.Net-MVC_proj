ad6e6d6a3c35d7f3d3c143544cdb6a74cc0e9d8e
Inga <inga.ziupsniene@gmail.com>
2019-11-11 13:21:48 +0200

    import project

---
+++ MVCProject/Models/EntityManager/UserManager.cs
@@ -0,0 +1,67 @@
+﻿using MVCProject.Models.DB;
+using MVCProject.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models.EntityManager
+{
+    public class UserManager
+    {
+        public void AddUserAccount(UserSignUpView user)
+        {
+            using (MVCProjectDBEntities db = new MVCProjectDBEntities())
+            {
+
+                SYSUser SU = new SYSUser();
+                SU.LoginName = user.LoginName;
+                SU.PasswordEncryptedText = user.Password;
+                SU.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SU.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1; ;
+                SU.RowCreatedDateTime = DateTime.Now;
+                SU.RowMOdifiedDateTime = DateTime.Now;
+
+                db.SYSUsers.Add(SU);
+                db.SaveChanges();
+
+                SYSUserProfile SUP = new SYSUserProfile();
+                SUP.SYSUserID = SU.SYSUserID;
+                SUP.FirstName = user.FirstName;
+                SUP.LastName = user.LastName;
+                SUP.Gender = user.Gender;
+                SUP.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SUP.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SUP.RowCreatedDateTime = DateTime.Now;
+                SUP.RowModifiedDateTime = DateTime.Now;
+
+                db.SYSUserProfiles.Add(SUP);
+                db.SaveChanges();
+
+
+                if (user.LOOKUPRoleID > 0)
+                {
+                    SYSUserRole SUR = new SYSUserRole();
+                    SUR.LOOKUPRoleID = user.LOOKUPRoleID;
+                    SUR.SYSUserID = user.SYSUserID;
+                    SUR.IsActive = true;
+                    SUR.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                    SUR.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                    SUR.RowCreatedDateTime = DateTime.Now;
+                    SUR.RowModifiedDateTime = DateTime.Now;
+
+                    db.SYSUserRoles.Add(SUR);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public bool IsLoginNameExist(string loginName)
+        {
+            using (MVCProjectDBEntities db = new MVCProjectDBEntities())
+            {
+                return db.SYSUsers.Where(o => o.LoginName.Equals(loginName)).Any();
+            }
+        }
+    }
+}
